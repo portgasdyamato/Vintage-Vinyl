b7dd8cd7e0be6ce87a5d60912d52494ea2606cb9
Vishy773 <sagar2111965@gmail.com>
2025-04-28 21:34:58 +0530

    Updated Navbar to toggle Queue visibility with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Play from './components/Play';
 import Navbar from './components/Navbar';
+import './App.css'; // Import the CSS file for styling
+import bgVideo from './assets/bg.mp4'; // Import the background video
 
 export default function App() {
     return (
         <>
+                        <div className="video-container">
+                <video className="background-video" autoPlay muted loop>
+                    <source src={bgVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
             <Navbar />
             <Play />
         </>
